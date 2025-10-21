2f1794318853245b68171eb00d037b0c942a1399
Phillip Wood <Phillipwood2023@outlook.com>
2025-10-21 18:54:10 -0400

    Fixed: the node placement location in the graph.

--- scripts/graph_system/graph.gd
+++ scripts/graph_system/graph.gd
@@ -1,17 +1,23 @@
 extends GraphEdit
 
-@export var context_menu_offset := Vector2i.ZERO ## The offset to spawn the contextual from the mouse cursor
+@export var context_menu_offset := Vector2.ZERO ## The offset to spawn the contextual from the mouse cursor
 
 @onready var context_menu = $"GraphNodeMenu"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	popup_request.connect(_on_popup_request)
+	context_menu.connect("_on_add_node", _on_add_node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func _on_popup_request(location : Vector2):
-	context_menu.position = Vector2i(location) + context_menu_offset
+	context_menu.position = location + context_menu_offset
 	context_menu.show()
+
+func _on_add_node(node : GraphNode):
+	node.position_offset = (get_local_mouse_position() + scroll_offset) / zoom + - node.size / 2
+	print(node.position_offset)
+	add_child(node)
--- scripts/graph_system/graph_node_menu.gd
+++ scripts/graph_system/graph_node_menu.gd
@@ -4,6 +4,9 @@ extends PopupMenu
 
 var ADD_NODE_ID := 0
 
+## SIGNALS
+signal _on_add_node(node : GraphNode)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	id_pressed.connect(_on_selection)
@@ -15,7 +18,5 @@ func _process(_delta: float) -> void:
 func _on_selection(id : int):
 	match id:
 		ADD_NODE_ID:
-			var graph = get_parent() as GraphEdit
 			var new_node = GraphNodeTypes[0].instantiate() as GraphElement
-			new_node.position_offset = Vector2i(graph.get_local_mouse_position()) - graph.context_menu_offset
-			graph.add_child(new_node)
+			_on_add_node.emit(new_node)
