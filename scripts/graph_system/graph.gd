92bdda378eb0facfa0ddeafd9ad57408c1fed330
Phillip Wood <Phillipwood2023@outlook.com>
2025-10-21 15:21:18 -0400

    Adding Nodes to graph via a context popup menu.

--- scripts/graph_system/graph.gd
+++ scripts/graph_system/graph.gd
@@ -1,11 +1,17 @@
 extends GraphEdit
 
+@export var context_menu_offset := Vector2i.ZERO ## The offset to spawn the contextual from the mouse cursor
+
+@onready var context_menu = $"GraphNodeMenu"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	popup_request.connect(_on_popup_request)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _on_popup_request(location : Vector2):
+	context_menu.position = Vector2i(location) + context_menu_offset
+	context_menu.show()
---
+++ scripts/graph_system/graph_node_menu.gd
@@ -0,0 +1,21 @@
+extends PopupMenu
+
+@export var GraphNodeTypes : Array[PackedScene]
+
+var ADD_NODE_ID := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	id_pressed.connect(_on_selection)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_selection(id : int):
+	match id:
+		ADD_NODE_ID:
+			var graph = get_parent() as GraphEdit
+			var new_node = GraphNodeTypes[0].instantiate() as GraphElement
+			new_node.position_offset = graph.get_local_mouse_position()
+			graph.add_child(new_node)
