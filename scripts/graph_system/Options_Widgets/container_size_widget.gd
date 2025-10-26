0f5d54b945664316e3f0ac76c506fb54bc5b7cef
Phillip Wood <Phillipwood2023@outlook.com>
2025-10-26 14:56:22 -0400

    Updated container size widget to use a label to represent the container size instead of a spinbox.

--- scripts/graph_system/Options_Widgets/container_size_widget.gd
+++ scripts/graph_system/Options_Widgets/container_size_widget.gd
@@ -2,19 +2,19 @@ extends HBoxContainer
 
 signal on_size_change(value)
 
-@onready var value_box : SpinBox = $"SpinBox"
+@onready var value_box : Label = $SizeLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"Decrement".pressed.connect(on_decrement)
 	$"Increment".pressed.connect(on_increment)
-	value_box.value_changed.connect(value_changed)
 
 func on_increment():
-	value_box.value += 1
+	var value = value_box.text as int
+	value_box.text = value + 1
+	on_size_change.emit(value)
 
 func on_decrement():
-	value_box.value -= 1
-
-func value_changed(value):
+	var value = value_box.text as int
+	value_box.text = value - 1
 	on_size_change.emit(value)
