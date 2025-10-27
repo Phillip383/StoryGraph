13f30ed3952303abb533e2581fc3dfec36f7e4c5
Phillip Wood <Phillipwood2023@outlook.com>
2025-10-27 18:23:47 -0400

    SCRUM-26 #Done The new node window checks the parent graph for nodes with a matching name, if one is found prompts a message and keeps the confirm button disabled. Also, a node can't be added without selecting a type, and there is a minimum length for a node name, stored in a constant.

--- scripts/graph_system/Nodes/new_node_window.gd
+++ scripts/graph_system/Nodes/new_node_window.gd
@@ -5,16 +5,27 @@ The types of graph nodes that can be added to the graph
 """
 @export var node_types : Dictionary[NodeData.NodeType, PackedScene]
 
+const REQUIRED_NAME_LENGTH = 2
+const NAME_LENGTH_MESSAGE = "The required length for node names is %d" % REQUIRED_NAME_LENGTH
+const EXISTS_MESSAGE = "Node already exists, choose another name."
+const NO_TYPE_MESSAGE = "A node need's a type, please select a type."
+
 var _selected_type : int
 var _name : StringName
+
+@onready var type_option : NodeTypeSelection = $MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/NodeType
+@onready var message_box : Label = $MarginContainer/CenterContainer/VBoxContainer/MessageBox
+@onready var confirm_button : Button = $MarginContainer/CenterContainer/VBoxContainer/Buttons/Confirm
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	close_requested.connect(_on_cancel_pressed)
-	pass # Replace with function body.
+	_selected_type = type_option.get_selected_id()
 
 func _on_type_selected(ID : int) -> void:
 	_selected_type = ID
+	confirm_button_state()
 
 func _on_cancel_pressed() -> void:
 	queue_free()
@@ -29,3 +40,31 @@ func _on_confirm_pressed() -> void:
 ## TODO: Check the graph for any node's with existing name, disable confirm button.
 func _on_name_changed(new_text: String) -> void:
 	_name = new_text
+	confirm_button_state()
+
+func confirm_button_state():
+	if _name.length() >= REQUIRED_NAME_LENGTH:
+		if _selected_type == type_option.get_default_id(): ## If user has made no selection.
+			message_box.visible = true
+			message_box.text = NO_TYPE_MESSAGE
+			confirm_button.disabled = true
+		elif check_for_existing_node():
+			message_box.visible = true
+			message_box.text = EXISTS_MESSAGE
+			confirm_button.disabled = true
+		else:
+			message_box.visible = false
+			confirm_button.disabled = false
+	else:
+		message_box.visible = true
+		message_box.text = NAME_LENGTH_MESSAGE
+		confirm_button.disabled = true
+
+func check_for_existing_node() -> bool:
+	var nodes = get_parent().get_children()
+	for node in nodes:
+		if node is BaseStoryNode:
+			if node.title == _name:
+				return true
+
+	return false
--- scripts/graph_system/Nodes/node_type_selection.gd
+++ scripts/graph_system/Nodes/node_type_selection.gd
@@ -2,6 +2,8 @@ extends OptionButton
 
 class_name NodeTypeSelection
 
+const DEFAULT_SELECTION_ID = 10
+
 ## Emitted when item is selected, passes the selected ID.
 signal on_type_selected(ID : int)
 
@@ -9,12 +11,18 @@ signal on_type_selected(ID : int)
 func _ready() -> void:
 	add_node_types()
 	item_selected.connect(on_selection)
+	selected = get_item_index(DEFAULT_SELECTION_ID)
+
+func get_default_id():
+	return DEFAULT_SELECTION_ID
 
 func add_node_types():
+	add_item("Type", DEFAULT_SELECTION_ID)
 	add_item("Start", NodeData.NodeType.ENTRY)
 	add_item("End", NodeData.NodeType.EXIT)
 	add_item("Transit", NodeData.NodeType.TRANSIT)
 	add_item("Link", NodeData.NodeType.LINK)
+	set_item_disabled(get_item_index(DEFAULT_SELECTION_ID), true)
 
 func on_selection(_index : int):
 	on_type_selected.emit(get_selected_id())
