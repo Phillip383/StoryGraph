86c17fa992e2e704a19968d3448621fac5144a71
Phillip Wood <Phillipwood2023@outlook.com>
2025-10-28 20:10:24 -0400

    Added the ability for "enter" to accept a level save. Validating name for existing level, and if name was provided.

--- scripts/Popups/save_window.gd
+++ scripts/Popups/save_window.gd
@@ -2,6 +2,10 @@ extends Window
 
 class_name SaveWindow
 
+const WARNING_POPUP = preload("res://scenes/UI/Popups/warning_popup.tscn")
+const WARNING_EXISTS = "Level with name already exists."
+const WARNING_NO_NAME = "Please Name The Level."
+
 signal on_save(file_name : String)
 
 var _file_name : String
@@ -16,6 +20,18 @@ func _on_cancel_pressed() -> void:
 	queue_free()
 
 func _on_save_pressed() -> void:
-	##TODO: Add validation, ensure the name is not a duplicate.
-	on_save.emit(_file_name)
-	queue_free()
+	if _file_name.length() > 0: 
+		if not FileAccess.file_exists(GraphEditor.get_levels_directory() + "/%s.json" % [_file_name]):
+			on_save.emit(_file_name)
+			queue_free()
+		else:
+			var warning_popup = WARNING_POPUP.instantiate()
+			add_child(warning_popup)
+			warning_popup.set_message(WARNING_EXISTS)
+	else:
+		var warning_popup = WARNING_POPUP.instantiate()
+		add_child(warning_popup)
+		warning_popup.set_message(WARNING_NO_NAME)
+
+func _on_filename_text_submitted(_new_text: String) -> void:
+	_on_save_pressed()
