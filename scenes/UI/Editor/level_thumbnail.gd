cd79da4ae92c7c20cde4ce861d32c791c81551ab
Phillip Wood <Phillipwood2023@outlook.com>
2025-11-02 10:57:13 -0500

    Added hover effect to level thumbnail.

--- scenes/UI/Editor/level_thumbnail.gd
+++ scenes/UI/Editor/level_thumbnail.gd
@@ -1,11 +1,17 @@
-extends VBoxContainer
+extends PanelContainer
 
 class_name LevelThumbnail
 
-@onready var _name_label : Label = $Name
+@export var hover_box : StyleBox
+@export var normal_box : StyleBox
+
+
+@onready var _name_label : Label = $HBox/Name
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	mouse_entered.connect(on_hovered)
+	mouse_exited.connect(on_hover_end)
 	pass # Replace with function body.
 
 func set_thumbnail_name(_name : String):
@@ -13,8 +19,13 @@ func set_thumbnail_name(_name : String):
 
 func _gui_input(event: InputEvent) -> void:
 	## TODO: Handle hover event's
-	## TODO: Open level on double click
 	if event is InputEventMouseButton and event.double_click:
 		var status = []
 		FileManager.load_file_by_name(_name_label.text, FileManager.FileType.LEVEL, status)
 		assert(status[0] == OK, "Level failed to load from thumbnail. :: Error: " + error_string(status[0]))
+
+func on_hovered():
+	add_theme_stylebox_override("panel", hover_box)
+
+func on_hover_end():
+	add_theme_stylebox_override("panel", normal_box)
